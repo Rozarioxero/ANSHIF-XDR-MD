dabf081c29e635eaa9446630712c85c441a4746a
Rozarioxero <akuiiiahuuu@gmail.com>
2024-10-30 11:46:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "aM11BJ7A#gqQ3AZesFfI2JzoVV1HuLCgWx_Bdq7gY6A_KO6yJwmM",
+MONGODB: process.env.MONGODB || "mongodb+srv://rush:20041025sa@clust era.ysi6dsf.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4270.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
